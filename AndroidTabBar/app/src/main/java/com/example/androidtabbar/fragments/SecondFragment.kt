7a8545e1c37a24bf6ac3f4d5cdfc8273c7df1682
Alex Golub <alexeythumb@gmail.com>
2020-12-29 19:32:45 +0200

    Move fragments files to a separate package

--- AndroidTabBar/app/src/main/java/com/example/androidtabbar/fragments/SecondFragment.kt
+++ AndroidTabBar/app/src/main/java/com/example/androidtabbar/fragments/SecondFragment.kt
@@ -1,4 +1,4 @@
-package com.example.androidtabbar
+package com.example.androidtabbar.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import com.example.androidtabbar.R
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
