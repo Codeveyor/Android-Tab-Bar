781eba0a180756296c85d78a4838c7015c1dea34
Alex Golub <alexeythumb@gmail.com>
2020-12-30 22:03:39 +0200

    Add Fragment extensions

---
+++ AndroidTabBar/app/src/main/java/com/example/androidtabbar/extensions/Fragment.kt
@@ -0,0 +1,41 @@
+package com.example.androidtabbar.extensions
+
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import androidx.fragment.app.Fragment
+import com.example.androidtabbar.R
+
+// TODO:
+// Use for custom ViewModels to send and observe
+//fun <T: ViewModel> Fragment.setupViewModel(viewModel: T) : T {
+//    val activity = requireActivity()
+//    return ViewModelProvider(
+//        MainApplication.viewModelStore,
+//        ViewModelProvider.AndroidViewModelFactory.getInstance(activity.application)
+//    ).get(viewModel::class.java)
+//}
+
+// Use for bottom navigation forward transition
+fun <T: Fragment> Fragment.pushBottomNavigation(navigationId: Int, fragment: T) {
+    requireActivity().supportFragmentManager.beginTransaction()
+        .setCustomAnimations(R.anim.slide_from_left_in, R.anim.slide_from_left_out)
+        .add(navigationId, fragment)
+        .addToBackStack(null)
+        .commit()
+}
+
+// Use for bottom navigation back transition
+fun <T: Fragment>Fragment.popBottomNavigation(fragment: T) {
+    val animation: Animation = AnimationUtils.loadAnimation(requireActivity(), R.anim.slide_from_right_out)
+    animation.duration = resources.getInteger(android.R.integer.config_shortAnimTime).toLong()
+    animation.setAnimationListener(object : Animation.AnimationListener {
+        override fun onAnimationStart(animation: Animation?) {}
+        override fun onAnimationRepeat(animation: Animation?) {}
+        override fun onAnimationEnd(animation: Animation?) {
+            val fragmentTransaction = requireActivity().supportFragmentManager.beginTransaction()
+            fragmentTransaction.remove(fragment)
+            fragmentTransaction.commitAllowingStateLoss()
+        }
+    })
+    requireView().startAnimation(animation)
+}
